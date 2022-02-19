a167778e428907c0ba7e0372a8d11d6bbc95a3bb
gf2206 <68312553+gf2206@users.noreply.github.com>
2022-02-19 13:09:51 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -41,7 +41,7 @@ def webServer(port=13331):
             except IOError:
         # Send response message for file not found (404)
         # Fill in start
-                connectionSocket.send("HTTP/1.1 200 OK \r\n 404 Not Found".encode())
+                connectionSocket.send("HTTP/1.1 200 OK \r\n 404 Not Found Try Again, lol!".encode())
         # Fill in end
 
         # Close client socket
